402c7bbf070fd16bf58ded1da2ae71bf51d5ed49
Julius van der Vaart <studio@juliusvaart.com>
2015-03-24 15:21:04 +0100

    v1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+(function ($, Drupal, window, document, undefined) {
+
+  Drupal.behaviors.my_custom_behavior = {
+    attach: function(context, settings) {
+      
+      // script
+      
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
