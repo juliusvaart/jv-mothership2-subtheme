8dd3ea2f77dbdc953462ca2ee29c73f71b4b7a2e
Julius van der Vaart <studio@juliusvaart.com>
2015-04-12 19:46:46 +0200

    404 page

---
+++ tpl/page--404.tpl.php
@@ -0,0 +1,23 @@
+<div class="page">
+
+  <article style="text-align: center;margin: 60px 0">
+
+    <h1 style="line-height: 1em !important">Page not found</h1><br/>
+
+    <p>The page you are trying to view does not exist (anymore).</p>
+    
+    <small>You will be redirected to the frontpage in 5 seconds.</small>
+    
+    <script type="text/javascript">
+      window.setTimeout(function(){
+        window.location.replace('/');
+      }, 5000);
+    </script>
+
+  </article>
+
+</div>
+
+<footer role="contentinfo">
+
+</footer>
